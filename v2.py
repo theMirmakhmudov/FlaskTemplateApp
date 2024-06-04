551d4b71b0b8d83924d11eefc48ee3605061b78d
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-06-04 13:07:22 +0500

    Commit

--- v2.py
+++ v2.py
@@ -7,8 +7,17 @@ app = Flask(__name__)
 def hello():
     return "Hello, I'm Flask"
 
+@app.route('/hello')
+def say_hello():
+    return "Hello World"
+
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
     app.run(debug=True)
+
+
+
+
+
