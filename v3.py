a0c8febb56d68232a941b526a7ca99a28c7913a4
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-06-04 13:10:37 +0500

    Commit

--- v3.py
+++ v3.py
@@ -3,15 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello, I'm Flask"
-
-
-@app.route('/hello')
 def say_hello():
-    return "Hello World"
+    return "Hello Flask"
 
+
+def say_bye():
+    return "Bye Flask"
+
+
+app.add_url_rule('/', 'hello', say_hello)
+app.add_url_rule('/hello','')
 
 if __name__ == "__main__":
     app.debug = True
