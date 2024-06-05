821d6e6eb50e6ca1a6286fccbc88f433166502ee
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-06-05 08:13:28 +0500

    Commit

---
+++ v7.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return redirect("/helloworld")
+
+
+@app.route("/helloworld")
+def hello_world():
+    return "<p>Hello, World from \
+                redirected page.!</p>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
