c459d67f217a0ea10f1921df2c6572a496049ecd
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-06-05 08:09:17 +0500

    Commit

--- v6.py
+++ v6.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/index', methods=['GET'])
+@app.route('/index')
 def index():
     return render_template('index.html')
 
 
 if (__name__ == "__main__"):
     app.run(debug=True)
+
+# show index.html and another html files
