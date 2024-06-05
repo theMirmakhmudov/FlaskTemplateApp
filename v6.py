6dae9e6ebbbcaa561caf282a87c56d0b819d6f09
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-06-05 08:07:09 +0500

    Commit

--- v6.py
+++ v6.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route('/square', methods=['GET'])
 def squarenumber():
-
     if request.method == 'GET':
 
         if (request.args.get('num') == None):
@@ -13,15 +12,12 @@ def squarenumber():
 
         elif (request.args.get('num') == ''):
             return "<html><body> <h1>Invalid number</h1></body></html>"
-
+        else:
             number = request.args.get('num')
             sq = int(number) * int(number)
-
             return render_template('answer.html',
                                    squareofnum=sq, num=number)
 
 
-# Start with flask web app with debug as
-# True only if this is the starting page
 if (__name__ == "__main__"):
     app.run(debug=True)
