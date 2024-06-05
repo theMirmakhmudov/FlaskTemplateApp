602a83b35489e515bc901f48edd40b73314c58d4
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-06-05 08:29:41 +0500

    Commit

---
+++ v10.py
@@ -0,0 +1,29 @@
+# another port run
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World! this application runing on 8000 port'
+
+
+if __name__ == '__main__':
+    app.run(port=8000)
+# -------------------------------------------------------------------------------------------------- #
+# host running flask
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World! this application runing on 192.168.0.105'
+
+
+if __name__ == '__main__':
+    app.run(host='192.168.0.105')
