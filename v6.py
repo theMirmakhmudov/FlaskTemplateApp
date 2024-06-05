eb309c20468bb28a2286de9fa5b3a97c5dc5bed4
mirmakhmudov16 <mr.mirmakhmudov16112008@gmail.com>
2024-06-05 08:08:18 +0500

    Commit

--- v6.py
+++ v6.py
@@ -3,20 +3,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/square', methods=['GET'])
-def squarenumber():
-    if request.method == 'GET':
-
-        if (request.args.get('num') == None):
-            return render_template('squarenum.html')
-
-        elif (request.args.get('num') == ''):
-            return "<html><body> <h1>Invalid number</h1></body></html>"
-        else:
-            number = request.args.get('num')
-            sq = int(number) * int(number)
-            return render_template('answer.html',
-                                   squareofnum=sq, num=number)
+@app.route('/index', methods=['GET'])
+def index():
+    return render_template('index.html')
 
 
 if (__name__ == "__main__"):
